5667c5280994443eed17ce3cee743c6869bcca88
Mina Borhani <92939258+MinaBorhani@users.noreply.github.com>
2022-03-15 01:24:43 +0330

    new commit

--- script/forecast.js
+++ script/forecast.js
@@ -1,2 +1,17 @@
 const key ='id9uFGTAS4AEnDqCBv3LgSuPMEAeAcG9';
 
+const getCity = async (city)=>{
+
+    const base ='http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response =await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+getCity('manchester')
+.then(data =>console.log(data))
+.catch(err => console.log(err));
