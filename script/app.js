b7a8968499d619f97761adec929fc43cdc913df2
Mina Borhani <92939258+MinaBorhani@users.noreply.github.com>
2022-03-16 21:30:46 +0330

    new

--- script/app.js
+++ script/app.js
@@ -1,16 +1,35 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+    const cityDats = data.cityDats;
+    const weather  = data.weather;
+
+    //update details template
+    details.innerHTML = `
+    <h5 class="my-3">${cityDats.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+    </div>
+    `;
+
+    //remove the d-non class if present
+    if(card.classList.contains('d-none'));
+    card.classList.remove('d-none');
+};
+
 
 const updatecity = async (city) =>{
 
-    console.log(city);
+    // console.log(city);
 
     const cityDats =await getCity(city);
     const weather = await getWeather(cityDats.Key);
 
-    return {
-        cityDats : cityDats,
-        weather : weather
-    }
+    return { cityDats, weather }
 };
 
 cityForm.addEventListener('submit' , e =>{
@@ -23,6 +42,7 @@ cityForm.addEventListener('submit' , e =>{
 
     //update the ui whith new city
     updatecity(city)
-    .then(data =>console.log(data))
+    // .then(data =>console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err));
 });
